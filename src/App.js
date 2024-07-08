4003b7e5f21b50bae9f2cfa72ff03c3872d9f881
Kaan Kani İnce <kkince@newmind.ai>
2024-07-08 10:47:20 +0300

    Qr-code-generator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import QrCodeGenerator from "./components/qr-code-generator";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* QR code generator*/}
+      <QrCodeGenerator/>
+    </div>
+  );
+}
+
+export default App;
